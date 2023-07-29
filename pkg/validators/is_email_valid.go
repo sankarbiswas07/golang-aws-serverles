600ff2f622bde784f11e627f77353aecd7ec7a50
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-29 19:48:23 +0530

    sample regular expression chek validation for email

---
+++ pkg/validators/is_email_valid.go
@@ -0,0 +1,16 @@
+package validator
+
+import (
+	"regexp"
+)
+
+func isValidEmail(email string) bool {
+	// Regular expression pattern for validating email addresses
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+
+	// Compile the regular expression
+	re := regexp.MustCompile(emailRegex)
+
+	// Use the MatchString function to check if the email matches the pattern
+	return re.MatchString(email)
+}
