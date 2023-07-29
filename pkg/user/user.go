ecb1414aa7f939165fee850cb86a8ebca17cf578
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-29 21:44:33 +0530

    FetchUsers function

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,19 +1,66 @@
 package user
 
 import(
+	"encoding/json"
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
+	
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 var(
-
+	ErrorFailedToFetchRecord = "failed to fetch record"
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal Dynamodb Scan record"
 )
 
-func FetchUser(email, tableName, dynaClient)(){
-
+type User struct{
+	Email string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
 }
 
-func FetchUsers()(){
-	
+func FetchUser(email string, tableName string, dynaClient *dynamodb.DynamoDB)(*User, error){
+	input := &dynamodb.GetItemInput{
+		Key: map[string] *dynamodb.AttributeValue{
+			"email": { 
+				S: aws.String(email)
+			}
+		},
+		TableName: aws.String(tableName)
+	}
+
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+	item := new(User)
+	err := dynamodbattribute.Unmarshal(page.Items, item)
+	if err != nil {
+				return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+
+	return item, nil
+}
+
+func FetchUsers(tableName string, dynaClient *dynamodb.DynamoDB)([]User, error){
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(tableName)
+	}
+
+	result, err := dynaClient.Scan(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+	item := new([]User)
+	err := dynamodbattribute.UnmarshalList(page.Items, item)
+	if err != nil {
+				return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+
+	return item, nil
 }
 
 func CreateUser()(){
